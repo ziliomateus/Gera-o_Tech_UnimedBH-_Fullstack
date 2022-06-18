d1ef9392198c3f47969cd36cdefdf762274900a6
Mateus Zilio <mateuszilio21@gmail.com>
2022-06-18 07:16:41 -0300

    arquivo4

---
+++ Modulo3_Conhecendo_Linguagem_Programação_JavaScript/Variaveis_e_Tipos/polindromo.js
@@ -0,0 +1,31 @@
+/*Atividade 1
+Verifique, de duas maneiras diferentes entre si, se uma String é um palíndromo.
+
+Palíndromo: frase ou palavra que se pode ler, indiferentemente, da esquerda para a direita ou vice-versa (ex.: raiar, ama, ovo, radar)
+*/
+
+
+
+
+// solução 1
+function verificaPalindromo(string) {
+	if (!string) return;
+	console.log(string === string.split('').reverse().join(''));
+}
+verificaPalindromo('cat');
+
+// solução 2
+
+function varificaPalindromo2(string) {
+	if (!string) return;
+	if (!string.length) return;
+
+	for (var i = 0; i < string.length / 2; i++) {
+		if (string[i] !== string[string.length - 1 - i]) {
+			return console.log(false);
+		}
+	}
+	return console.log(true);
+}
+
+varificaPalindromo2('asa');
---
+++ Modulo3_Conhecendo_Linguagem_Programação_JavaScript/Variaveis_e_Tipos/substituiPares.js
@@ -0,0 +1,29 @@
+/* Atividade 2
+Troque todos os elementos pares e diferentes de zero de um array pelo número 0. Se o array for vazio, retorne -1.
+
+Exemplo: Input -> [1, 3, 4, 6, 80, 33, 23, 90]
+
+Output -> [1, 3, 0, 0, 0, 33, 23, 0]
+
+Input -> []
+
+Output -> -1
+*/
+function substituiNumerosPares(array) {
+	if (!array.length) return console.log(-1);
+
+	const naoZero = (num) => num !== 0;
+	const numPar = (num) => num % 2 === 0;
+
+	for (let i = 0; i < array.length; i++) {
+		if (numPar(array[i]) && naoZero(array[i])) {
+			console.log(`trocando ${array[i]} por 0...`);
+			array[i] = 0;
+		} else if (!naoZero(array[i])) {
+			console.log('Ops, você já vale 0!');
+		}
+	}
+	console.log(array);
+}
+
+substituiNumerosPares([2, 0, 3, 5]);
